599f3eabd94c8dc8f84f14421cecf60561a78dfc
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-04-19 23:56:37 +0100

    create project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Header } from './components/header'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,3 @@
+export function Header() {
+  return <strong>Ignite Feed</strong>
+}
